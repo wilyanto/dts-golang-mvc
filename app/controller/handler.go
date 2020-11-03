5dcf175965ac9651f325c4651e37741166f66eff
wilyanto <wilyanto24@gmail.com>
2020-11-03 12:22:07 +0700

    add handler to all controllers

--- app/controller/handler.go
+++ app/controller/handler.go
@@ -1,9 +1,107 @@
 package controller
 
-// import (
-// 	"github.com/gin-gonic/gin"
-// )
+import (
+	"github.com/gin-gonic/gin"
+	"DTS_IT_Perbankan_Back_End/Digitalent-Kominfo_Implementation-MVC-Golang/app/utils"
+	"DTS_IT_Perbankan_Back_End/Digitalent-Kominfo_Implementation-MVC-Golang/app/model"
+)
 
-// func (inDB *InDB) CreateAccount(c *gin.Context) {
-	
-// }
+func (inDB *InDB) CreateAccount(c *gin.Context) {
+	var account model.Account
+	if err := c.Bind(&account); err!= nil {
+		utils.WrapAPIError(c,err.Error(),http.StatusBadRequest)
+		return
+	}
+	pass, err := utils.HashGenerator(account.Password); if err != nil{
+		utils.WrapAPIError(c,err.Error(),http.StatusBadRequest)
+		return
+	}
+	account.Password = pass
+	flag,err := model.InsertNewAccount(account)
+	if flag{
+		utils.WrapAPISuccess(c,"success",http.StatusOK)
+		return
+	}else {
+		utils.WrapAPIError(c,err.Error(),http.StatusBadRequest)
+		return
+	}
+}
+
+func GetAccount (c *gin.Context){
+	idAccount := c.MustGet("account_number").(int)
+	flag,err,trx,acc := model.GetAccountDetail(idAccount);if err != nil {
+		utils.WrapAPIError(c,err.Error(),http.StatusInternalServerError)
+		return
+	}
+	if flag{
+		utils.WrapAPIData(c,map[string]interface{}{
+			"account":acc,
+			"transaction":trx,
+		},http.StatusOK,"success")
+		return
+	}
+}
+
+func Transfer (c *gin.Context){
+	var transaction model.Transaction
+	if err := c.Bind(&transaction); err!= nil {
+		utils.WrapAPIError(c,err.Error(),http.StatusBadRequest)
+		return
+	}
+
+	flag,err := model.Transfer(transaction); if flag{
+		utils.WrapAPISuccess(c,"success",http.StatusOK)
+		return
+	}else {
+		utils.WrapAPIError(c,err.Error(),http.StatusBadRequest)
+		return
+	}
+}
+
+func Withdraw (c *gin.Context){
+	var transaction model.Transaction
+	if err := c.Bind(&transaction); err!= nil {
+		utils.WrapAPIError(c,err.Error(),http.StatusBadRequest)
+		return
+	}
+
+	flag,err := model.Withdraw(transaction); if flag{
+		utils.WrapAPISuccess(c,"success",http.StatusOK)
+		return
+	}else {
+		utils.WrapAPIError(c,err.Error(),http.StatusBadRequest)
+		return
+	}
+}
+
+func Deposit (c *gin.Context){
+	var transaction model.Transaction
+	if err := c.Bind(&transaction); err!= nil {
+		utils.WrapAPIError(c,err.Error(),http.StatusBadRequest)
+		return
+	}
+
+	flag,err := model.Deposit(transaction); if flag{
+		utils.WrapAPISuccess(c,"success",http.StatusOK)
+		return
+	}else {
+		utils.WrapAPIError(c,err.Error(),http.StatusBadRequest)
+		return
+	}
+}
+
+func Login(c *gin.Context){
+	var auth model.Auth
+	if err := c.Bind(&auth); err != nil {
+		utils.WrapAPIError(c,err.Error(),http.StatusBadRequest)
+		return
+	}
+	log.Println("LOGIN")
+	flag,err,token := model.Login(auth); if flag{
+		utils.WrapAPIData(c,map[string]interface{}{
+			"token":token,
+		},http.StatusOK,"success")
+	}else {
+		utils.WrapAPIError(c,err.Error(),http.StatusBadRequest)
+	}
+}
--- app/model/bank.go
+++ app/model/bank.go
@@ -2,11 +2,13 @@ package model
 
 import (
 	"fmt"
+	"DTS_IT_Perbankan_Back_End/Digitalent-Kominfo_Implementation-MVC-Golang/app/constant"
+	"DTS_IT_Perbankan_Back_End/Digitalent-Kominfo_Implementation-MVC-Golang/app/utils"
 	"github.com/dgrijalva/jwt-go"
 	"gorm.io/gorm"
 	"github.com/pkg/errors"
-	// "DTS_IT_Perbankan_Back_End/Digitalent-Kominfo_Implementation-MVC-Golang/app/constant"
-	"DTS_IT_Perbankan_Back_End/Digitalent-Kominfo_Implementation-MVC-Golang/app/utils"
+	"log"
+	"time"
 )
 
 type Account struct {
@@ -26,14 +28,16 @@ type Auth struct {
 }
 
 type Transaction struct {
+	DB *gorm.DB
 	ID int `gorm:"primary_key" json:"-"`
 	IdAccountRefer int `json:"-"`
 	IdTransaction string `json:"id_transaction"`
 	TransactionType int `json:"transaction_type"`
 	TransactionDescription string `json:"transaction_description"`
 	Sender int `json:"sender"`
+	Amount int `json:"amount"`
 	Recipient int `json:"recipient"`
-	Timestamp int `json:"timestamp"`
+	Timestamp int64 `json:"timestamp"`
 }
 
 func Login(auth Auth) (bool, error, string) {
@@ -94,4 +98,80 @@ func (account Account) GetAccountDetail(idAccount int) (bool,error, []Transactio
 	}
 
 	return true,nil, transaction, account
+}
+
+func (trx Transaction) Transfer () (bool,error){
+
+	err := trx.DB.Transaction(func(tx *gorm.DB) error {
+		// do some database operations in the transaction (use 'tx' from this point, not 'db')
+		var sender,recipient Account
+		if err := tx.Model(&Account{}).Where(&Account{AccountNumber: trx.Sender}).
+			First(&sender).
+			Update("saldo", sender.Saldo-trx.Amount).Error; err != nil {
+			// return any error will rollback
+			return err
+		}
+		if err := tx.Model(&Account{}).Where(&Account{AccountNumber: trx.Recipient}).
+			First(&recipient).
+			Update("saldo", recipient.Saldo+trx.Amount).Error; err != nil {
+			// return any error will rollback
+			log.Println("ERROR : " + err.Error())
+			return err
+		}
+		trx.TransactionType = constant.TRANSFER
+		trx.Timestamp = time.Now().Unix()
+		if err := tx.Create(&trx).Error;err != nil {
+			return err
+		}
+		// return nil will commit the whole transaction
+		return nil
+	});if err != nil {
+		return false, err
+	}
+	return true,nil
+}
+
+func Withdraw (transaction Transaction) (bool,error){
+	err := transaction.DB.Transaction(func(tx *gorm.DB) error {
+		var sender Account
+		if err := tx.Model(&Account{}).Where(&Account{AccountNumber: transaction.Sender}).
+			First(&sender).
+			Update("saldo", sender.Saldo-transaction.Amount).Error; err != nil {
+			// return any error will rollback
+			return err
+		}
+		transaction.TransactionType = constant.WITHDRAW
+		transaction.Timestamp = time.Now().Unix()
+		if err := tx.Create(&transaction).Error;err != nil {
+			return err
+		}
+		return nil
+	});if err != nil {
+		return false, err
+	}
+
+	return true,nil
+}
+
+func Deposit (transaction Transaction) (bool,error){
+	err := transaction.DB.Transaction(func(tx *gorm.DB) error {
+		var sender Account
+		if err := tx.Model(&Account{}).Where(&Account{AccountNumber: transaction.Sender}).
+			First(&sender).
+			Update("saldo", sender.Saldo+transaction.Amount).Error; err != nil {
+			// return any error will rollback
+			return err
+		}
+		transaction.TransactionType = constant.DEPOSIT
+		transaction.Timestamp = time.Now().Unix()
+		if err := tx.Create(&transaction).Error;err != nil {
+			// return any error will rollback
+			return err
+		}
+		return nil
+	});if err != nil {
+		return false, err
+	}
+
+	return true,nil
 }
